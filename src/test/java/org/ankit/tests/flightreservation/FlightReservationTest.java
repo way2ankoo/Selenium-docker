4fbc15dc57b80a713387f7d5cbebfb12eefeaf22
ankit <way2ankoo@gmail.com>
2024-07-07 22:08:48 +0530

    create test for flight reservation

---
+++ src/test/java/org/ankit/tests/flightreservation/FlightReservationTest.java
@@ -0,0 +1,76 @@
+package org.ankit.tests.flightreservation;
+
+import org.ankit.pages.flightreservation.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class FlightReservationTest {
+
+    private WebDriver driver;
+    private String noOfPassengers;
+    private String expectedPrice;
+
+    @BeforeTest
+    @Parameters({"noOfPassengers", "expectedPrice"})
+    public void setDriver(String noOfPassengers, String expectedPrice){
+        this.noOfPassengers = noOfPassengers;
+        this.expectedPrice = expectedPrice;
+
+        //driver setup
+//        WebDriverManager.chromedriver().setup();
+        this.driver = new ChromeDriver();
+    }
+
+    @Test
+    public void userRegistrationTest(){
+        RegistrationPage registrationPage = new RegistrationPage(driver);
+        registrationPage.goT0("https://d1uh9e7cu07ukd.cloudfront.net/selenium-docker/reservation-app/index.html");
+        Assert.assertTrue(registrationPage.isAt());
+
+        registrationPage.enterUserDetails("selenium", "docker");
+        registrationPage.enterUserCredentials("delenium@docker.com", "docker");
+        registrationPage.enterAddress("nirala green sector 2", "noida", "201307");
+        registrationPage.register();
+    }
+
+    @Test(dependsOnMethods = "userRegistrationTest")
+    public void registrationConfirmationTest(){
+        RegistrationConfirmationPage registrationConfirmationPage = new RegistrationConfirmationPage(driver);
+        Assert.assertTrue(registrationConfirmationPage.isAt());
+        registrationConfirmationPage.goToFlightSearchButton();
+    }
+
+    @Test(dependsOnMethods = "registrationConfirmationTest")
+    public void flightSearchTest(){
+        FlightSearchPage flightSearchPage = new FlightSearchPage(driver);
+        Assert.assertTrue(flightSearchPage.isAt());
+        flightSearchPage.selectPassenger(noOfPassengers);
+        flightSearchPage.searchFlight();
+    }
+
+    @Test(dependsOnMethods = "flightSearchTest")
+    public void flightSelectionTest(){
+        FlightSelectionPage flightSelectionPage = new FlightSelectionPage(driver);
+        Assert.assertTrue(flightSelectionPage.isAt());
+        flightSelectionPage.selectFlights();
+        flightSelectionPage.confirmFlight();
+
+    }
+
+    @Test(dependsOnMethods = "flightSelectionTest")
+    public void flightConfirmationTest(){
+        FlightConfirmationPage flightConfirmationPage = new FlightConfirmationPage(driver);
+        Assert.assertTrue(flightConfirmationPage.isAt());
+        Assert.assertEquals(flightConfirmationPage.getPrice(), expectedPrice);
+    }
+
+    @AfterTest
+    public void quitDriver(){
+        this.driver.quit();
+    }
+}
